6996cf1d2fdf8f394116bbd72374f0a37e65bde1
Simon Melouah <simon.melouah@mail.dcu.ie>
2016-03-05 22:00:55 +0000

    dateutil removed

--- models.py
+++ models.py
@@ -1,19 +1,11 @@
 #This python file builds all sql tables using sqlalchemy
-# import pyodbc;
-from sqlalchemy import *;
-from sqlalchemy.orm import sessionmaker;
-from sqlalchemy.ext.declarative import declarative_base;
-import urllib;
-from datetime import datetime, timedelta;
-from dateutil import tz;
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import urllib
+from datetime import datetime, timedelta
 
-#Proper method of getting accurate local time
-from_zone = tz.tzutc()
-to_zone = tz.tzlocal()
-utc = datetime.utcnow()
-utc = utc.replace(tzinfo = from_zone)
-central = utc.astimezone(to_zone)
-offset = int(central.strftime("%z")) 
+
 Base = declarative_base()
 
 class Users(Base):
@@ -22,7 +14,6 @@ class Users(Base):
     username = Column(String(50))
     password = Column(String(150))
 
-#Classes for sql tables
 class Projects(Base):
     __tablename__ = 'Projects'
     id = Column(Integer, primary_key = True)
